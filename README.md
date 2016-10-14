63cab2817b8e9bcbd6e461bb79275e356a67f56e
Ally Boyd <alyssa.boyd85@gmail.com>
2016-10-14 15:02:50 -0500

    Add some class notes and update README

---
+++ README.md
@@ -0,0 +1,13 @@
+# CRUD Pet Hotel
+
+This app takes pet owner information from a form and inserts it into a database table. It also takes information on pets and inserts it into another database table. As new pets are added, they are appended to the DOM.
+
+## Future updates
+
+In the future, the Update and Delete buttons will be operational. The update button will update the information on the DOM and in the table. The Delete button will remove that pet's information from the visits table, and then also the pets table and the DOM. There will also be a change to the check-in button. When the button is clicked, check-in information will be send to the database table, and the button will change to check-out. Then, when the check-out button is clicked, it will update the database table again and change back into a check-in button.
+
+## Authors:
+
+- Ally Boyd
+- Jackie Torborg
+- Andy Wolff
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,13 +8,13 @@ $(function () {
 
   $('.petRegister').on('click', petRegister);
 
-  //$('.update').on('click', updatePet);
+  $('.update').on('click', updatePet);
+
   $('.petInfoTable').on('click', '.delete', deletePet);
+
   //$('.check-in').on('click', checkinPet);
 
-
 });
-
 
 //button to update pet -- updates pets table
 //
@@ -26,7 +26,7 @@ $(function () {
 //
 //$('.dropdown').controlgroup();
 
-function ownerRegister (event) {
+function ownerRegister(event) {
   event.preventDefault();
   var $ownerButton = $(this);
   var $ownerForm = $ownerButton.closest('#registrationForm');
@@ -41,7 +41,7 @@ function ownerRegister (event) {
   $(this).find('input').val('');
 }
 
-function petRegister (event) {
+function petRegister(event) {
   event.preventDefault();
   var $petButton = $(this);
   var $petFormInfo = $petButton.closest('#petsForm');
@@ -56,43 +56,52 @@ function petRegister (event) {
   $(this).find('input').val('');
 }
 
-function getInfo () {
+function getInfo() {
   $.ajax({
     type: 'GET',
     url: '/owners',
-    success: displayInfo
+    success: displayInfo,
   });
 }
-function getPetInfo () {
+
+function getPetInfo() {
   $.ajax({
     type: 'GET',
     url: '/pets',
-    success: displayPetInfo
+    success: displayPetInfo,
   });
 }
 
 function displayInfo(response) {
   console.log(response);
-  var $list = $('.dropdownList');
-  $list.empty();
-  response.forEach(function(owner) {
-    $list.append('<option>' + owner.first_name + ' ' + owner.last_name + '</option>');
+  var $dropdown = $('.dropdownList');
+  $dropdown.empty();
+  response.forEach(function (owner) {
+    var $appendOption = $('<option>' + owner.first_name + ' ' + owner.last_name + '</option>');
+    $dropdown.append($appendOption);
+    $appendOption.info('id', owner.id);
   });
-
 }
+
 function displayPetInfo(response) {
   console.log(response);
+
+  // TO DO: turn table data that should be editable into inputs
+
   var $table = $('.petInfoTable');
   $table.empty();
   $table.append('<tr><th>Owner</th><th>Pet</th><th>Breed</th><th>Color</th><th>Update</th><th>Delete</th><th>Check In/Out</th></tr>');
-  response.forEach(function(pet) {
-     var $deleteButton = $('<button class="delete">Delete</button>');
-     $deleteButton.data('id', pet.id);
+  response.forEach(function (pet) {
+    var $deleteButton = $('<button class="delete">Delete</button>');
+    $deleteButton.data('id', pet.id);
     var $tr = $('<tr id="petRow"></tr>');
     $tr.append('<td>' + pet.owner_id + '</td>');
     $tr.append('<td>' + pet.name + '</td>');
     $tr.append('<td>' + pet.breed + '</td>');
     $tr.append('<td>' + pet.color + '</td>');
+
+    //TO DO: change update button so it's like the delete button
+
     $tr.append('<td><button class="update">Update</button></td>');
 
     var $td = $('<td></td>');
@@ -106,28 +115,24 @@ function displayPetInfo(response) {
 }
 
 //function updatePet() {
-
+// TO DO: find the input(type text).val for each element and change on DOM and in database table when updated
 //console.log('inside updatePet');
 
 //}
 
 function deletePet(event) {
 
-console.log('inside deletePet');
-//console.log('Pet id:', pet.id);
-event.preventDefault();
-var petId = $(this).data('id');//we need to add data to something
-console.log('petId variable', petId);
-
-
-$.ajax({
-type: 'DELETE',
-url: '/pets/' + petId,
-success: displayPetInfo
-
-
-})
-
+  console.log('inside deletePet');
+  //TO DO: put data directly on the button when appended
+  //console.log('Pet id:', pet.id);
+  event.preventDefault();
+  var petId = $(this).data('id');//we need to add data to something
+  console.log('petId variable', petId);
+  $.ajax({
+    type: 'DELETE',
+    url: '/pets/' + petId,
+    success: displayPetInfo,
+  });
 }
 
 //function checkinPet() {
